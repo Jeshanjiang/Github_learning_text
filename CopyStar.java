824fd3249084a07cea0d40a51b9390c6d59a6fc2
jiaxiong jiang <jiaxiongjiang@jiaxiongs-MacBook-Pro.local>
2024-09-20 15:57:42 -0700

    this is good

---
+++ CopyStar.java
@@ -0,0 +1,27 @@
+// Jiaxiong Jiang
+
+// August 18 2024
+
+// Lab1 CopyStar 
+
+public class CopyStar
+{
+   public static void main (String[] args)
+   {
+      System.out.println ("*****************");
+      System.out.println ("*              * *");
+      System.out.println ("*              *  *");
+      System.out.println ("*              *   *");
+      System.out.println ("*              *    *");
+      System.out.println ("*              *    *");
+      System.out.println ("*              *    *");
+      System.out.println ("*              *    *");
+      System.out.println ("****************    *");
+      System.out.println ("*               *   *");
+      System.out.println (" *               *  *");
+      System.out.println ("  *               * *");
+      System.out.println ("   *               **");
+      System.out.println ("    *****************");
+      
+   }
+}
