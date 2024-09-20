528fe24002a35bd680f4633279ecf58c9984b691
jiaxiong jiang <jiaxiongjiang@jiaxiongs-MacBook-Pro.local>
2024-09-20 15:04:15 -0700

    frist commit

---
+++ CalculateBill.java
@@ -0,0 +1,21 @@
+// Jiaxiong Jiang
+
+// August 20 2024
+
+// Lab 2 CalculateBill 
+
+// This program calcultates a customer's bill
+
+public class CalculateBill 
+{
+   public static void main (String [] args)
+   {
+      int charges = 100;
+      int saleTax = 12;
+      int totalOwed = charges + saleTax;
+      System.out.println (" You purchased $ " + charges 
+                          + ". Sale tax is $ " + saleTax
+                          + ". You owe $ " + totalOwed + ".");
+    }
+}
+   
