b0be7ab9c818016681ac1e11ee02ddc6f6096fa5
jiaxiong jiang <jiaxiongjiang@jiaxiongs-MacBook-Pro.local>
2024-10-04 11:49:55 -0700

    my frist commit

---
+++ Methods.java
@@ -0,0 +1,20 @@
+// Jiaxiong Jiang Sergio Mario Arista Basil Franklin
+
+// October 2 2024
+
+// Lab 10  Method
+
+// The method should tell if the integers are  “in order”.
+
+
+
+public class Methods
+{
+   public boolean isInOrder (int x,int y,int z)
+   {
+      return x < y && y < z;
+      
+   }
+}
+
+
